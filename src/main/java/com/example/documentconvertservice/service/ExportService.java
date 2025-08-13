61119d8f28d63106970955b8e91552a659c1e5f6
Gilbert1Rosa <gilbertli2033@gmail.com>
2025-08-13 11:57:57 -0400

    - Little Refator to ExportService.

--- src/main/java/com/example/documentconvertservice/service/ExportService.java
+++ src/main/java/com/example/documentconvertservice/service/ExportService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import javax.imageio.*;
 import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
+import javax.print.Doc;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
@@ -26,10 +27,13 @@ public class ExportService {
     private static final List<Document> documents = new ArrayList<>();
 
     public InputStream getExport(int resolution) throws IOException {
+
+        if (documents.isEmpty()) {
+            throw new IllegalArgumentException("No documents uploaded");
+        }
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ImageWriter writer = ImageIO.getImageWritersByFormatName("TIFF").next();
-
-        ImageReader reader = ImageIO.getImageReadersByFormatName("TIFF").next();
 
         try (ImageOutputStream imageOutputStream = ImageIO.createImageOutputStream(outputStream)) {
             writer.setOutput(imageOutputStream);
@@ -41,47 +45,33 @@ public class ExportService {
             writer.prepareWriteSequence(null);
 
             int tiffIndex = 0;
-            int documentIndex = 0;
-            int totalDocuments = documents.size();
+            int totalPages = 0;
+
             for (Document document : documents) {
-                webSocketHandler.sendProgress((double) documentIndex / totalDocuments * 100);
-                if (document.getType() == Document.DocumentType.TIFF) {
-                    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(document.getContent());
-                    try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(byteArrayInputStream)) {
-                        reader.setInput(imageInputStream);
-
-                        int numImages = reader.getNumImages(true);
-
-                        for (int imageIndex = 0; imageIndex < numImages; imageIndex++) {
-                            BufferedImage image = reader.read(imageIndex);
-                            writer.writeInsert(tiffIndex, new IIOImage(image, null, null), params);
-                            tiffIndex++;
+                switch (document.getType()) {
+                    case PDF -> {
+                        try (PDDocument pdDocument = PDDocument.load(document.getContent())) {
+                            totalPages += pdDocument.getNumberOfPages();
                         }
                     }
-                } else if (document.getType() == Document.DocumentType.PDF) {
-                    try (PDDocument pdDocument = PDDocument.load(document.getContent())) {
-                        PDFRenderer pdfRenderer = new PDFRenderer(pdDocument);
-                        int pageCount = pdDocument.getNumberOfPages();
-
-                        int start = document.getStartPage();
-                        int stop = Math.min(document.getEndPage(), pageCount - 1);
-
-                        if (start < 0 || stop < 0) {
-                            throw new IllegalArgumentException("Start and stop page cannot be negative");
-                        }
-
-                        for (int pageIndex = start; pageIndex <= stop; pageIndex++) {
-                            BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(
-                                    pageIndex,
-                                    resolution,
-                                    ImageType.RGB
-                            );
-                            writer.writeInsert(tiffIndex, new IIOImage(bufferedImage, null, null), params);
-                            tiffIndex++;
+                    case TIFF -> {
+                        ImageReader reader = ImageIO.getImageReadersByFormatName("TIFF").next();
+                        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(document.getContent());
+                        try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(byteArrayInputStream)) {
+                            reader.setInput(imageInputStream);
+                            totalPages += reader.getNumImages(true);
                         }
                     }
                 }
-                documentIndex++;
+            }
+
+            for (Document document : documents) {
+                webSocketHandler.sendProgress((double) tiffIndex / totalPages * 100);
+                if (document.getType() == Document.DocumentType.TIFF) {
+                    tiffIndex = readTIFF(document, writer, params, tiffIndex);
+                } else if (document.getType() == Document.DocumentType.PDF) {
+                    tiffIndex = readPDF(document, writer, params, tiffIndex, resolution);
+                }
             }
 
             writer.endWriteSequence();
@@ -111,5 +101,48 @@ public class ExportService {
         details.setDocuments(documents);
         details.setTotalSize(totalSize);
         return details;
+    }
+
+    private int readTIFF(Document document, ImageWriter writer, ImageWriteParam params, int tiffIndex) throws IOException {
+        ImageReader reader = ImageIO.getImageReadersByFormatName("TIFF").next();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(document.getContent());
+        try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(byteArrayInputStream)) {
+            reader.setInput(imageInputStream);
+
+            int numImages = reader.getNumImages(true);
+
+            for (int imageIndex = 0; imageIndex < numImages; imageIndex++) {
+                BufferedImage image = reader.read(imageIndex);
+                writer.writeInsert(tiffIndex, new IIOImage(image, null, null), params);
+                tiffIndex++;
+            }
+        }
+
+        return tiffIndex;
+    }
+
+    private int readPDF(Document document, ImageWriter writer, ImageWriteParam params, int tiffIndex, int resolution) throws IOException {
+        try (PDDocument pdDocument = PDDocument.load(document.getContent())) {
+            PDFRenderer pdfRenderer = new PDFRenderer(pdDocument);
+            int pageCount = pdDocument.getNumberOfPages();
+
+            int start = document.getStartPage();
+            int stop = Math.min(document.getEndPage(), pageCount - 1);
+
+            if (start < 0 || stop < 0) {
+                throw new IllegalArgumentException("Start and stop page cannot be negative");
+            }
+
+            for (int pageIndex = start; pageIndex <= stop; pageIndex++) {
+                BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(
+                        pageIndex,
+                        resolution,
+                        ImageType.RGB
+                );
+                writer.writeInsert(tiffIndex, new IIOImage(bufferedImage, null, null), params);
+                tiffIndex++;
+            }
+        }
+        return tiffIndex;
     }
 }
